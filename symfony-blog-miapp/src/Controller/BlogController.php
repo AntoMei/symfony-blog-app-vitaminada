8ecdb505d1944f0151a8d1fa72596be15f5775c5
AntoMei <antonio.meirosu444@gmail.com>
2022-11-14 13:31:53 +0100

    Finalizado hasta el punto 1.2.3 Reto - Rutas /contact, /blog y /single_post

---
+++ symfony-blog-miapp/src/Controller/BlogController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogController extends AbstractController
+{
+    #[Route('/blog', name: 'app_blog')]
+    public function index(): Response
+    {
+        return $this->render('blog/blog.html.twig', [
+            'controller_name' => 'BlogController',
+        ]);
+    }
+
+     /**
+     * @Route("/single_post", name="single_post")
+     */
+    public function post(): Response
+    {
+        return $this->render('blog/single_post.html.twig', [
+            'controller_name' => 'BlogController',
+        ]);
+    }
+}
