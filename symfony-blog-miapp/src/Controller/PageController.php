7466fd522eed75f00e1e54cba03a6c9643bea9dc
AntoMei <antonio.meirosu444@gmail.com>
2022-11-18 12:38:45 +0100

    Falta añadir el estilo del mensaje y realizado hasta el punto 2.2.2

--- symfony-blog-miapp/src/Controller/PageController.php
+++ symfony-blog-miapp/src/Controller/PageController.php
@@ -5,6 +5,10 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
+use App\Entity\Contact;
+use Symfony\Component\HttpFoundation\Request;
+use App\Form\ContactFormType;
 
 class PageController extends AbstractController
 {
@@ -26,11 +30,21 @@ class PageController extends AbstractController
         return $this->render('page/about.html.twig', []);
     }
 
-    /**
-    * @Route("/contact", name="contact")
-    */
-    public function contact(): Response
-    {
-        return $this->render('page/contact.html.twig', []);
+    #[Route('/contact', name: 'contact')]
+public function contact(ManagerRegistry $doctrine, Request $request): Response
+{
+    $contact = new Contact();
+    $form = $this->createForm(ContactFormType::class, $contact);
+    $form->handleRequest($request);
+    if ($form->isSubmitted() && $form->isValid()) {
+        $contacto = $form->getData();    
+        $entityManager = $doctrine->getManager();    
+        $entityManager->persist($contacto);
+        $entityManager->flush();
+        return $this->redirectToRoute('index', []);
     }
+    return $this->render('page/contact.html.twig', array(
+        'form' => $form->createView()    
+    ));
+}
 }
